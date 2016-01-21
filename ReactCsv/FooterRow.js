ce888352344469fb17f5b72d4c3771b1d063ef72
David Timmons <github@timmons.io>
2016-01-21 16:24:42 -0600

    Cleaned up the FooterRow class format.

--- ReactCsv/FooterRow.js
+++ ReactCsv/FooterRow.js
@@ -3,19 +3,30 @@
  * David Timmons (github@timmons.io)
  * http://david.timmons.io
  * MIT License
+ *
+ * @summary An optional component controlling the table footer.
+ * @module ReactCsv/FooterRow
  */
 
 import React from 'react';
 import Cell from './Cell.js';
 
 
-// (Optional) Spreadsheet footer row controlled via <Sheet> with <hasFooter>.
-var FooterRow = React.createClass({
-  propTypes: {
-    saveChange: React.PropTypes.func.isRequired,
-    csv: React.PropTypes.arrayOf(React.PropTypes.array.isRequired)
-  },
-  render: function() {
+/**
+ * (Optional) Spreadsheet footer row controlled via <Sheet> with <hasFooter>.
+ * @extends React.Component
+ */
+export default class FooterRow extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  /**
+   * Default React render function.
+   * @return {object} A reference to the DOM component.
+   */
+  render() {
     var rows = this.props.numRows;
     var cells = Array(this.props.numCols).fill(0).map((val, i) =>
       <td className="p0 border-top border-right" key={10*rows + i} data-row={rows} onBlur={this.props.saveChange}>
@@ -30,6 +41,14 @@ var FooterRow = React.createClass({
       </tfoot>
     );
   }
-});
+}
 
-export default FooterRow;
+/**
+ * Restrict the property types.
+ * @type {object}
+ * @memberof FooterRow
+ */
+FooterRow.propTypes = {
+  saveChange: React.PropTypes.func.isRequired,
+  csv: React.PropTypes.arrayOf(React.PropTypes.array.isRequired)
+};
