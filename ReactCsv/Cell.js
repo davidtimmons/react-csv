7c1db078978cdb4990ba23234ad019e9c280ae5c
David Timmons <github@timmons.io>
2016-01-21 16:54:13 -0600

    Cleaned up the Cell class format.

--- ReactCsv/Cell.js
+++ ReactCsv/Cell.js
@@ -3,34 +3,56 @@
  * David Timmons (github@timmons.io)
  * http://david.timmons.io
  * MIT License
+ *
+ * @summary A component controlling the table cells.
+ * @module ReactCsv/Cell
  */
 
 import React from 'react';
 
 
-// Spreadsheet cells comprised of tiny form elements.
-var Cell = React.createClass({
-  propTypes: {
-    classHead: React.PropTypes.string,
-    classFoot: React.PropTypes.string,
-    csv: React.PropTypes.any
-  },
-  getInitialState: function() {
-    return {
+/**
+ * Spreadsheet cells comprised of tiny form elements.
+ * @extends React.Component
+ */
+export default class Cell extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
       styles: this.props.classHead || this.props.classFoot || ''
     };
-  },
-  handleSubmit: function(e) {
+  }
+
+  /**
+   * Prevent form submission and exit input mode.
+   * @param {object} e A DOM event object.
+   */
+  _handleSubmit(e) {
     e.preventDefault(); /// Prevent form submit.
     document.activeElement.blur(); /// Unfocus this cell.
-  },
-  render: function() {
+  }
+
+  /**
+   * Default React render function.
+   * @return {object} A reference to the DOM component.
+   */
+  render() {
     return (
-      <form className="m0 p0 container block" onSubmit={this.handleSubmit}>
+      <form className="m0 p0 container block" onSubmit={this._handleSubmit}>
         <input type="text" className={"col-12 border-none " + this.state.styles} value={this.props.csv} />
       </form>
     );
   }
-});
+}
 
-export default Cell;
+/**
+ * Restrict the property types.
+ * @type {object}
+ * @memberof Toolbar
+ */
+Cell.propTypes = {
+  classHead: React.PropTypes.string,
+  classFoot: React.PropTypes.string,
+  csv: React.PropTypes.any
+};
