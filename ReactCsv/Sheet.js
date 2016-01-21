3aeb4a3e29c1d7822a1cf7808298a5bdd67c67be
David Timmons <github@timmons.io>
2016-01-21 02:36:39 -0600

    Converted Sheet to ES2015 class syntax.

--- ReactCsv/Sheet.js
+++ ReactCsv/Sheet.js
@@ -16,21 +16,20 @@ import Toolbar from './Toolbar.js';
 
 
 // Primary high-level component that orchestrates pieces and maintains state.
-var Sheet = React.createClass({
-  propTypes: {
-    numCols: React.PropTypes.number,
-    numRows: React.PropTypes.number,
-    hasFooter: React.PropTypes.bool,
-    showExportButton: React.PropTypes.bool
-  },
-  getInitialState: function() {
-    return {
+export default class Sheet extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    // This is equivalent to <getInitialState()>.
+    this.state = {
       tableUndo: [],
       tableRedo: [],
       table: this.createEmptyTable()
     };
-  },
-  componentDidMount: function() {
+  }
+
+  componentDidMount() {
     // Listen for keystrokes after the component attaches to the document.
     document.onkeydown = function(e) {
       // Undo: CTRL-Z | Redo: CTRL-Y
@@ -40,40 +39,45 @@ var Sheet = React.createClass({
         this.redo();
       }
     }.bind(this);
-  },
-  createEmptyTable: function() {
+  }
+
+  createEmptyTable() {
     // Create an empty 2D array of the specified size.
     var table = [];
     for (let i = 0, len = this.props.numRows; i < len; i++) {
       table.push(Array(this.props.numCols).fill(null));
     }
     return table;
-  },
-  undo: function() {
+  }
+
+  undo() {
     // Undo the current data state.
     this.setState((prevState) => {return {
       tableUndo: update(prevState.tableUndo, {$splice: [[0, 1]]}),
       tableRedo: update(prevState.tableRedo, {$unshift: [JSON.stringify(prevState.table)]}),
       table: JSON.parse(prevState.tableUndo[0])
     }});
-  },
-  redo: function() {
+  }
+
+  redo() {
     // Restore the previous data state.
     this.setState((prevState) => {return {
       tableUndo: update(prevState.tableUndo, {$unshift: [JSON.stringify(prevState.table)]}),
       tableRedo: update(prevState.tableRedo, {$splice: [[0, 1]]}),
       table: JSON.parse(prevState.tableRedo[0])
     }});
-  },
-  reset: function() {
+  }
+
+  reset() {
     // Clear all data and destroy the redo queue.
     this.setState((prevState) => {return {
       tableUndo: update(prevState.tableUndo, {$unshift: [JSON.stringify(prevState.table)]}),
       tableRedo: [],
       table: this.createEmptyTable()
     }});
-  },
-  saveChange: function(e) {
+  }
+
+  saveChange(e) {
     // Change nothing if the value is empty.
     switch (e.target.value) {
       case '':
@@ -89,12 +93,14 @@ var Sheet = React.createClass({
       tableRedo: [],
       table: update(prevState.table, {[rowIndex]: {$splice: [[colIndex, 1, e.target.value]]}})
     }});
-  },
-  render: function () {
+  }
+
+  render() {
     const cols = this.props.numCols;
     const rows = this.props.numRows;
     const table = this.state.table;
-    const save = this.saveChange;
+    const save = this.saveChange.bind(this);
+    const reset = this.reset.bind(this);
     return (
       <div>
         <table className="table-light overflow-hidden bg-white border">
@@ -102,10 +108,15 @@ var Sheet = React.createClass({
           <BodyRows numCols={cols} numRows={this.props.hasFooter ? rows-2 : rows-1} csv={table} saveChange={save} />
           {this.props.hasFooter ? <FooterRow numCols={cols} numRows={rows-1} csv={table} saveChange={save} /> : null}
         </table>
-        <Toolbar csv={table} reset={this.reset} showExport={this.props.showExportButton} />
+        <Toolbar csv={table} reset={reset} showExport={this.props.showExportButton} />
       </div>
     );
   }
-});
+}
 
-export default Sheet;
+Sheet.propTypes = {
+  numCols: React.PropTypes.number,
+  numRows: React.PropTypes.number,
+  hasFooter: React.PropTypes.bool,
+  showExportButton: React.PropTypes.bool
+};
